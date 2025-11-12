cec5becb3bbb0b4974e547815b34727beeef32e7
Lbojor <a01799749@tec.mx>
2025-11-11 19:01:50 -0600

    Initial commit

---
+++ Actividad 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float base, altura, area;
+
+    cout << "Dame la base: " << endl;
+
+    cin >> base;
+
+    cout << "Dame la altura: " << endl;
+
+    cin >> altura;
+
+    area = base * altura;
+
+    cout << "La area es: " << area << endl;
+
+    system("pause");
+
+    return 0;
+
+}
